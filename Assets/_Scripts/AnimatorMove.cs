f48f4b71a5378418d6487da332327499774d5f25
Fan Chou <Chou.Fan@igt.com>
2015-10-12 10:20:44 +0800

    6.4.3 Animator Move. add mesh collider to Environments

---
+++ Assets/_Scripts/AnimatorMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorMove : MonoBehaviour {
+	public float directionDampTime =.25f;
+
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+
+		if (stateInfo.IsName ("Base Layer.Run")) {
+			if (Input.GetButton("Fire1")) {
+				anim.SetBool ("Jump", true);
+			}
+		}else{
+			anim.SetBool ("Jump", false);
+		}
+
+		if (Input.GetButton ("Fire2")) {
+			anim.SetBool ("Hi", !anim.GetBool("Hi"));
+		}
+
+		float speed = Mathf.Sqrt (h*h + v*v);
+		anim.SetFloat ("Speed", speed);
+		anim.SetFloat ("Direction", h, directionDampTime, Time.deltaTime);
+	}
+}
