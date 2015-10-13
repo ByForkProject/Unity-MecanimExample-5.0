255a131840a11e797eb836a17c798d3819867140
Fan Chou <Chou.Fan@igt.com>
2015-10-13 10:01:04 +0800

    6.4 Crowd Movement and Crowd Generation.

---
+++ Assets/PlayerGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGenerator : MonoBehaviour {
+	public GameObject teddy;
+	public GameObject dude;
+	public int showCount = 0;
+	public int maxPlayerCount = 50;
+	static int count = 0;
+	static float lastTime = 0f;
+	static float timeSpan = .1f;
+	// Use this for initialization
+	void Start () {
+		lastTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (count < maxPlayerCount) {
+			bool fired = Input.GetButton ("Fire1");
+
+			if ((Time.time - lastTime)> timeSpan) {
+				if (fired) {
+					Instantiate (teddy, Vector3.zero, Quaternion.identity);
+				}else{
+					Instantiate (dude, Vector3.zero, Quaternion.identity);
+				}
+				lastTime = Time.time;
+				count++;
+				showCount = count;
+			}
+		}
+	}
+}
---
+++ Assets/_Scripts/CrowdMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrowdMovement : MonoBehaviour {
+	public float AvatarRange = 25;
+
+	private Animator anim;
+	private float SpeedDampTime = .25f;
+	private float DirectionDampTime = .25f;
+	private Vector3 TargetPosition = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int r = Random.Range (0, 50);
+
+		anim.SetBool ("Jump", r == 20);
+		anim.SetBool ("Dive", r == 30);
+
+		if (Vector3.Distance (TargetPosition, anim.rootPosition) > 5) {
+			//Speed
+			anim.SetFloat ("Speed", 1, SpeedDampTime, Time.deltaTime);
+
+			//Direction
+			Vector3 currentDirection = anim.rootRotation * Vector3.forward;
+			Vector3 wantedDirection = (TargetPosition - anim.rootPosition).normalized;
+
+			if (Vector3.Dot (currentDirection, wantedDirection) >  0) { // < 90
+				anim.SetFloat ("Direction", 
+				               Vector3.Cross (currentDirection, wantedDirection).y, 
+				               DirectionDampTime,
+				               Time.deltaTime);
+			}else{ // > 90
+				anim.SetFloat ("Direction",
+				               Vector3.Cross (currentDirection, wantedDirection).y>0?1f:-1f, // left hand rules, > 0 turn right, < 0 turn left
+				               DirectionDampTime,
+				               Time.deltaTime);
+			}
+		}else{
+			anim.SetFloat ("Speed", 0, SpeedDampTime, Time.deltaTime);
+
+			if (anim.GetFloat ("Speed") < 0.01f) {
+				TargetPosition = new Vector3(
+					Random.Range (-AvatarRange, AvatarRange),
+					0,
+					Random.Range (-AvatarRange, AvatarRange)
+					);
+			}
+		}
+	}
+}
